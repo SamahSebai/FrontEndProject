6d0ac5e9d4c29b0baf153c7b69d4b318267294b6
ahlem tbini <74101630+ahlemtbini@users.noreply.github.com>
2023-05-25 20:01:04 +0100

    Update Demande_ expert.cy.js

--- cypress/e2e/Ahlem_test/Demande_ expert.cy.js
+++ cypress/e2e/Ahlem_test/Demande_ expert.cy.js
@@ -1,19 +1,22 @@
 describe('expert request', () => {
 it("create a expert request", () => {
-    cy.loginAsAlumni("tes1@gmail.com", "12345678");
+    cy.loginAsAlumni("AhlemTestAlumni@gmail.com", "123456");
     cy.get('a#Demandes.link').click();
+    cy.wait(2000);
     cy.location("pathname").should("eq", "/demande");
   
     cy.get('select#type1').select("expert");
     cy.get('textarea#compétences').type("Skills needed for expert request");
     cy.get('button[type="submit"]').click();
+    cy.wait(2000);
     cy.location("pathname").should("eq", "/profile");
   });
   it("consult a expert request", () => {
     cy.login("finance@bt.com", "123456");
     cy.get('a#expert_requests.link').click();
+    cy.wait(2000);
     cy.location("pathname").should("eq", "/Dexpert");
     cy.contains("Skills needed for expert request")
-  });
+  }); 
 });
-  
+   
