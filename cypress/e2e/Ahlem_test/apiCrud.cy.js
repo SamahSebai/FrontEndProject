cac40b2f4b869ada982545058671fc91df6c5eaa
ahlem tbini <74101630+ahlemtbini@users.noreply.github.com>
2023-05-27 02:59:41 +0100

    Update apiCrud.cy.js

--- cypress/e2e/Ahlem_test/apiCrud.cy.js
+++ cypress/e2e/Ahlem_test/apiCrud.cy.js
@@ -1,63 +1,79 @@
+describe("Blog API", () => {
+  let blogId;
+  let authToken;
 
-describe("API CRUD Operations", () => {
-    let createdBlogId;
-    let token;
-  
-    before(() => {
-      // Perform the login and obtain the token
-      cy.loginAsAlumni("AhlemTestAlumni@gmail.com", "123456").then(() => {
-        token = window.localStorage.getItem("token");
-  
-        // Create a new blog and store the created blog ID
-        cy.request({
-          method: "POST",
-          url: "http://localhost:4000/Api/V1/Blog/",
-          headers: {
-            Authorization: token,
-          },
-          body: {
-            Type: "Conseil",
-            Sujet: "New Blog Title",
-            description: "This is a new blog post!",
-            Moderateur:"642ffd1e4d5d1a38d97d8df8",
-          },
-          failOnStatusCode: false,
-        }).then((response) => {
-          expect(response.status).to.equal(201);
-          createdBlogId = response.body.id;
-        });
-      });
-    });
-  
-    it("updates a blog", () => {
-      cy.request({
-        method: "PUT",
-        url: `http://localhost:4000/Api/V1/Blog/${createdBlogId}`,
-        headers: {
-          Authorization: token,
-        },
-        body: {
-          Type: "Conseil",
-          Sujet: "New Updated Blog Title",
-          description: "This is a new Updated blog post!",
-        },
-        failOnStatusCode: false,
-      }).then((response) => {
-        expect(response.status).to.equal(200);
-      });
-    });
-  
-    it("deletes a blog", () => {
-      cy.request({
-        method: "DELETE",
-        url: `http://localhost:4000/Api/V1/Blog/${createdBlogId}`,
-        headers: {
-          Authorization: token,
-        },
-        failOnStatusCode: false,
-      }).then((response) => {
-        expect(response.status).to.equal(200);
-      });
+  beforeEach(() => {
+    cy.loginAsAlumni("AhlemTestAlumni@gmail.com", "123456").then(() => {
+      authToken = window.localStorage.getItem('token');
     });
   });
-  
+
+  it("creates a new blog", () => {
+    cy.request({
+      method: "POST",
+      url: "/Blog",
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: {
+        title: "New Blog",
+        content: "This is a new blog post.",
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      blogId = response.body._id;
+    });
+  });
+
+  it("fetches all blogs", () => {
+    cy.request({
+      method: "GET",
+      url: "/Blog",
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    });
+  });
+
+  it("fetches a blog by ID", () => {
+    cy.request({
+      method: "GET",
+      url: `/Blog/${blogId}`,
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    });
+  });
+
+  it("updates a blog", () => {
+    cy.request({
+      method: "PUT",
+      url: `/Blog/${blogId}`,
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: {
+        title: "Updated Blog",
+        content: "This is an updated blog post.",
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    });
+  });
+
+  it("deletes a blog", () => {
+    cy.request({
+      method: "DELETE",
+      url: `/Blog/${blogId}`,
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    });
+  });
+});
